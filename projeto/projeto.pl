754029bb3d27bc41fa3aa886711ecd877bec8eac
R-Camacho <rodrigompcamacho@gmail.com>
2023-12-08 23:34:33 +0000

    TODO: mudar nomes

--- projeto/projeto.pl
+++ projeto/projeto.pl
@@ -29,3 +29,35 @@ vizinhancaAlargada((L,C), [(N,O), (N,C), (N,E), (L,O), (L,E), (S,O), (S,C), (S,E
     E is C + 1, %este
     S is L + 1. %sul
 
+/*tamanhoTabuleiro/2
+Predicado auxiliar para calcular o tamanho de um tabuleiro (supondo que será sempre quadrada)
+tamanhoTabuleiro(Tab, N) é verdade se 
+*/
+
+/*todasCelulas/2
+todasCelulas(Tabuleiro, TodasCelulas) é verdade se TodasCelulas é uma lista orde-
+nada de cima para baixo e da esquerda para a direita, sem elementos repetidos, com todas as
+coordenadas do tabuleiro Tabuleiro;
+*/
+
+% Base case: matriz vazia não tem coordenadas
+todasCelulas([], []).
+
+% Recursão - chama o for loop exterior
+todasCelulas(Tabuleiro, Coordinates) :-
+    todasCelulasExterior(Tabuleiro, 1, Coordinates). % for loop "exterior"
+
+% Predicado que vai iterar sobre as linhas 
+todasCelulasExterior([], _, []).
+todasCelulasExterior([Row|RestRows], RowIndex, AllCoordinates) :-
+    todasCelulasInterior(Row, RowIndex, 1, RowCoordinates), % for loop "interior"
+    NextRowIndex is RowIndex + 1,
+    todasCelulasExterior(RestRows, NextRowIndex, RestCoordinates),
+    append(RowCoordinates, RestCoordinates, AllCoordinates).
+
+% Predicado que vai iterar sobre os elementos (colunas) numa linha 
+todasCelulasInterior([], _, _, []).
+todasCelulasInterior([_|RestCols], RowIndex, ColIndex, [(RowIndex, ColIndex)|RestCoordinates]) :-
+    NextColIndex is ColIndex + 1,
+    todasCelulasInterior(RestCols, RowIndex, NextColIndex, RestCoordinates).
+
