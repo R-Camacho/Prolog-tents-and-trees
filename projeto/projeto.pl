216c79db614023c64148a32de8906dee2b1367a9
R-Camacho <rodrigompcamacho@gmail.com>
2023-12-17 00:34:56 +0000

    4.1 done

--- projeto/projeto.pl
+++ projeto/projeto.pl
@@ -45,7 +45,6 @@ todasCelulas(Tabuleiro, TodasCelulas) :-
     %começamos a contar no 1 (nth1) porque no jogo o ponto superior esquerdo é (1, 1)
 
 
-
 /*todasCelulas/3
 todasCelulas(Tabuleiro, TodasCelulas, Objecto) é verdade se TodasCelulas é uma
 lista ordenada de cima para baixo e da esquerda para a direita, sem elementos repetidos,
@@ -65,9 +64,24 @@ todasCelulas((Tabuleiro, TodasCelulas, Objecto)) :-
 /*contaObjectos/3
 Predicado auxiliar: contaObjectos(O, L, C) é verdade se existem C vezes o objeto O na lista L
 */
-contaObjectos((Objecto), L, C) :-
-    include((==(Objecto)), L, L2),
-    length(L2, C).
+
+%Caso o Objecto seja uma variavel
+
+contaObjectos(Objecto, L, Conta) :- 
+    var(Objecto), !,  
+    setof(Objecto, member(Objecto, L), L2),
+    length(L2, Conta), !.
+
+
+%Caso o Objecto seja uma constante
+contaObjectos(Objecto, L, Conta) :-  
+
+    exclude(var, L, Constantes), %retira as variaveis da lista
+    findall(Objecto, member(Objecto, Constantes), L2),
+    length(L2, Conta), !.
+
+
+
 
 /*contaObjectosLinha/3
 Predicado auxiliar: contaObjectosLinha(O, Tabuleiro, ContaLinhas) é verdade se a 
@@ -88,42 +102,61 @@ contaObjectosColuna(Tabuleiro, Objecto, ContagemColunas):-
 calculaObjectosTabuleiro(Tabuleiro, ContagemLinhas, ContagemColunas, Objecto) é verdade se 
 */
 
+
+%caso especial - o objeto e uma variavel
+%calculaObjectosTabuleiro(Tabuleiro, ContagemLinhas, ContagemColunas, Objecto) :-
+
+
+
+
 calculaObjectosTabuleiro(Tabuleiro, ContagemLinhas, ContagemColunas, Objecto) :-
     contaObjectosLinha(Tabuleiro, Objecto, ContagemLinhas), 
-    contaObjectosColuna(Tabuleiro, Objecto, ContagemColunas).
+    transpose(Tabuleiro, Transposto), % contar as colunas de uma matriz e o mesmo que contar as linhas da sua transposta
+    contaObjectosLinha(Transposto, Objecto, ContagemColunas).
 
     
 /*celulaVazia/2
-celulaVazia(Tabuleiro, (L, C)) é verdade se Tabuleiro for um tabuleiro que não tem
+celulaVazia(Tabuleiro, (L, C)) e verdade se Tabuleiro for um tabuleiro que nao tem
 nada ou tem relva nas coordenadas (L, C)
 */
 celulaVazia(Tabuleiro, (L, C)) :-
     nth1(L, Tabuleiro, Linha), %mais uma vez comecamos a contar no 1
     nth1(C, Linha, Celula),% Aceder a linha L e depois coluna C
     member(Celula, [r|_]).
-     %ve se Celula e relva (r) ou esta vazia (neste caso uma variavel)
+    %ve se Celula e relva (r) ou esta vazia (neste caso uma variavel)
     
     
 /*insereObjectoCelula/3
-insereObjectoCelula(Tabuleiro, TendaOuRelva, (L, C)) é verdade se Tabuleiro é um
-tabuleiro e (L, C) são as coordenadas onde queremos inserir o objecto TendaOuRelva
+insereObjectoCelula(Tabuleiro, TendaOuRelva, (L, C)) e verdade se Tabuleiro e um
+tabuleiro e (L, C) sao as coordenadas onde queremos inserir o objecto TendaOuRelva
 */
 
 
 insereObjectoCelula(Tabuleiro, TendaOuRelva, (L, C)):-
     nth1(L, Tabuleiro, Linha),
     nth1(C, Linha, Celula),
-    ((nonvar(Celula)); nth1(C, Linha, TendaOuRelva, _)), !. 
+    ((nonvar(Celula)); nth1(C, Linha, TendaOuRelva)), !. 
     %se a celula estiver ocupada (for uma constante), T unifica consigo proprio 
-    %caso contrario é colocada a TendaOuRelva no Tabuleiro
+    %caso contrario e colocada a TendaOuRelva no Tabuleiro
     
 
 
 /*
 insereObjectoEntrePosicoes(Tabuleiro, TendaOuRelva, (L, C1), (L, C2)) é ver-
-dade se Tabuleiro é um tabuleiro, e (L, C1) e (L, C2) são as coordenadas, na Linha L,
+dade se Tabuleiro e um tabuleiro, e (L, C1) e (L, C2) sao as coordenadas, na Linha L,
 entre as quais (incluindo) se insere o objecto TendaOuRelva.
 */
-insereObjectoEntrePosicoes(Tabuleiro, TendaOuRelva, (L, C1), (L, C2)).
+insereObjectoEntrePosicoes(Tabuleiro, TendaOuRelva, (L, C1), (L, C2)):-
+    insereObjectoEntrePosicoes(Tabuleiro, TendaOuRelva, (L, C1), (L, C2), C1), !.
 
-%Ideia: identificar coluna ou linha a modificar e usar predicado anterior
+insereObjectoEntrePosicoes(_, _, (L, _), (L, C2), Prox_C2):-
+    Prox_C2 is C2 + 1. %Para quando a ultima coluna for ultrapassada
+
+insereObjectoEntrePosicoes(Tabuleiro, TendaOuRelva, (L, C1), (L, C2), Contador):-
+    Atual_C is Contador,
+    insereObjectoCelula(Tabuleiro, TendaOuRelva, (L, Atual_C)),
+    Prox_C is Contador + 1,
+    insereObjectoEntrePosicoes(Tabuleiro, TendaOuRelva, (L, C1), (L, C2), Prox_C).
+
+
+
